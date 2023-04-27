4ec2954af1d05cfd8cc4bc1825c6520b0f838c9c
Joe Ebmeier <joe.ebmeier@firespring.com>
2023-04-27 10:21:57 -0500

    WIP

--- firespring_dev_commands.gemspec
+++ firespring_dev_commands.gemspec
@@ -41,4 +41,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'rubocop-performance', '~> 1.15.0'
   s.add_development_dependency 'simplecov', '~> 0.21.0'
   s.add_development_dependency 'yard', '~> 0.9.28'
+  s.add_development_dependency 'launchy', '~> 2.5.2'
 end
--- lib/firespring_dev_commands/templates/docker/default.rb
+++ lib/firespring_dev_commands/templates/docker/default.rb
@@ -16,9 +16,10 @@ module Dev
             desc 'Builds all images used by services in the compose files' \
                  "\n\toptionally specify NO_CACHE=true to not use build cache" \
                  "\n\toptionally specify PULL=true to force pulling new base images"
-            task build: %w(init_docker) do
+            task build: %w(init_docker _pre_build_hooks) do
               LOG.debug('In base build')
               Dev::Docker::Compose.new.build
+              Rake::Task[:_post_build_hooks].execute
             end
           end
         end
@@ -32,9 +33,10 @@ module Dev
 
             desc 'Starts containers for all services listed in the compose files' \
                  "\n\toptionally specify DETACHED=false to not detach from the started services"
-            task up: %w(init_docker) do
+            task up: %w(init_docker _pre_up_hooks) do
               LOG.debug('In base up')
               Dev::Docker::Compose.new.up
+              Rake::Task[:_post_up_hooks].execute
             end
           end
         end
@@ -49,9 +51,10 @@ module Dev
             desc 'Connects to the output stream of all running containers' \
                  "\n\toptionally specify NO_FOLLOW=true to print all current output and exit" \
                  "\n\toptionally specify TAIL=n to print the last 'n' lines of output"
-            task logs: %w(init_docker) do
+            task logs: %w(init_docker _pre_logs_hooks) do
               LOG.debug('In base logs')
               Dev::Docker::Compose.new.logs
+              Rake::Task[:_post_logs_hooks].execute
             end
           end
         end
@@ -65,9 +68,10 @@ module Dev
 
             desc 'Stops and removes containers and associated resources' \
                  "\n\toptionally specify REMOVE_VOLUMES=true to also remove unused volumes"
-            task down: %w(init_docker) do
+            task down: %w(init_docker _pre_down_hooks) do
               LOG.debug('In base down')
               Dev::Docker::Compose.new.down
+              Rake::Task[:_post_down_hooks].execute
             end
           end
         end
@@ -80,8 +84,8 @@ module Dev
             return if exclude.include?(:reload)
 
             desc 'Runs a "down" followed by an "up"'
-            task reload: %w(down up) do
-              LOG.debug('In reload')
+            task reload: %w(_pre_reload_hooks down up) do
+              Rake::Task[:_post_reload_hooks].execute
             end
           end
         end
@@ -94,10 +98,11 @@ module Dev
             return if exclude.include?(:clean)
 
             desc 'Removes all stopped containers and unused images, volumes, and networks'
-            task clean: %w(init_docker) do
+            task clean: %w(init_docker _pre_clean_hooks) do
               LOG.debug 'In base clean'
               Dev::Docker.new.prune
               LOG.info
+              Rake::Task[:_post_clean_hooks].execute
             end
             task prune: [:clean] do
               # This is an alias to the clean command
@@ -113,9 +118,10 @@ module Dev
             return if exclude.include?(:push)
 
             desc 'Push all local images to their defined image repository'
-            task push: %w(init_docker) do
+            task push: %w(init_docker _pre_push_hooks) do
               LOG.debug 'In base push'
               Dev::Docker::Compose.new.push
+              Rake::Task[:_post_push_hooks].execute
             end
           end
         end
@@ -128,9 +134,10 @@ module Dev
             return if exclude.include?(:pull)
 
             desc 'Pull all images from their defined image repository'
-            task pull: %w(init_docker) do
+            task pull: %w(init_docker _pre_pull_hooks) do
               LOG.debug 'In base pull'
               Dev::Docker::Compose.new.pull
+              Rake::Task[:_post_pull_hooks].execute
             end
           end
         end
@@ -144,8 +151,9 @@ module Dev
 
             desc 'Print a table of all images' \
                  "\n\t(equivalent to docker images)"
-            task images: %w(init_docker) do
+            task images: %w(init_docker _pre_images_hooks) do
               Dev::Docker.new.print_images
+              Rake::Task[:_post_images_hooks].execute
             end
           end
         end
@@ -159,38 +167,9 @@ module Dev
 
             desc 'Print a table of all running containers' \
                  "\n\t(equivalent to docker ps)"
-            task ps: %w(init_docker) do
+            task ps: %w(init_docker _pre_ps_hooks) do
               Dev::Docker.new.print_containers
-            end
-          end
-        end
-
-        # Create the rake task which shows all docker images on the system
-        def create_images_task!
-          exclude = @exclude
-
-          DEV_COMMANDS_TOP_LEVEL.instance_eval do
-            unless exclude.include?(:images)
-              desc 'Print a table of all images' \
-                   "\n\t(equivalent to docker images)"
-              task images: [:init] do
-                Dev::Docker.new.print_images
-              end
-            end
-          end
-        end
-
-        # Create the rake task which shows all docker containers on the system
-        def create_ps_task!
-          exclude = @exclude
-
-          DEV_COMMANDS_TOP_LEVEL.instance_eval do
-            unless exclude.include?(:ps)
-              desc 'Print a table of all running containers' \
-                   "\n\t(equivalent to docker ps)"
-              task ps: [:init] do
-                Dev::Docker.new.print_containers
-              end
+              Rake::Task[:_post_ph_hooks].execute
             end
           end
         end
