d13dcc224d9218d26952d02934a37f84612d8a7a
Joe Ebmeier <joe.ebmeier@firespring.com>
2023-05-04 09:25:28 -0500

    [FDP-44548] Add stop functionality back to dev_commands library

--- lib/firespring_dev_commands/aws/profile.rb
+++ lib/firespring_dev_commands/aws/profile.rb
@@ -50,6 +50,15 @@ module Dev
         puts "    export AWS_SESSION_TOKEN=#{ENV.fetch('AWS_SESSION_TOKEN', nil)}"
         puts
       end
+
+      # Print the export commands for the current credentials
+      def export_info
+        Dev::Aws::Credentials.new.export!
+        puts "export AWS_DEFAULT_REGION=#{ENV.fetch('AWS_DEFAULT_REGION', nil)}"
+        puts "export AWS_ACCESS_KEY_ID=#{ENV.fetch('AWS_ACCESS_KEY_ID', nil)}"
+        puts "export AWS_SECRET_ACCESS_KEY=#{ENV.fetch('AWS_SECRET_ACCESS_KEY', nil)}"
+        puts "export AWS_SESSION_TOKEN=#{ENV.fetch('AWS_SESSION_TOKEN', nil)}"
+      end
     end
   end
 end
