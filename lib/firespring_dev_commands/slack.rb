d4d94ea62aa55c326e082d30a9692c4b60f62a91
Joe Ebmeier <joe.ebmeier@firespring.com>
2023-07-12 12:12:32 -0500

    WIP

--- lib/firespring_dev_commands/slack.rb
+++ lib/firespring_dev_commands/slack.rb
@@ -1,10 +1,28 @@
+require 'slack'
+
 module Dev
   class Slack
+    attr_accessor :client
+
     def initialize
-      # Test Auth
+      @client = ::Slack::Web::Client.new
+      @client.auth_test
     end
 
-    # D1KEH93LY
-    def post_message; end
+    def post(channel:, text:)
+      client.chat_postMessage(channel: channel, text: text)
+    end
+
+    def upload(channel:, title:, content:, filename: nil)
+      raise 'content should be a string' unless content.is_a?(String)
+
+      client.files_upload(
+        channels: channel,
+        title: title,
+        file: Faraday::UploadIO.new(StringIO.new(content), 'text/plain'),
+        filename: filename,
+        filetype: 'text'
+      )
+    end
   end
 end
