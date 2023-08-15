51b22ac635321bf85d717b9830f88bedddb3cce9
Joe Ebmeier <joe.ebmeier@firespring.com>
2023-08-15 13:19:05 -0500

    WIP

--- lib/firespring_dev_commands/version.rb
+++ lib/firespring_dev_commands/version.rb
@@ -6,6 +6,6 @@ module Dev
     # Use 'v.v.v.pre.alpha.v' for pre-release vesions
     # Use 'v.v.v.beta.v for beta versions
     # Use semantic versioning for any releases (https://semver.org/)
-    VERSION = '2.1.1.pre.alpha.4'.freeze
+    VERSION = '2.1.1.pre.alpha.5'.freeze
   end
 end
