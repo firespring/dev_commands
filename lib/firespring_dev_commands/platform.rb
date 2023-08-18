b0f9bdd7173f2551220acc06c83bb73df8346ede
Ron Gallagher <ron.gallagher@firespring.com>
2023-08-18 08:03:05 -0500

    Update

--- lib/firespring_dev_commands/platform.rb
+++ lib/firespring_dev_commands/platform.rb
@@ -1,7 +1,7 @@
 module Dev
   class Common
     class Platform
-      ALLOWED_ARCHITECTURES = %w[arm64 amd64]
+      ALLOWED_ARCHITECTURES = %w(arm64 amd64).freeze
 
       def determine_compute_architecture
         case RUBY_PLATFORM
@@ -10,7 +10,7 @@ module Dev
         when /arm|aarch64/
           'linux/arm64' # ARM architecture
         else
-          raise "Unknown or unsupported architecture"
+          raise 'Unknown or unsupported architecture'
         end
       end
 
@@ -19,17 +19,16 @@ module Dev
         if docker_architecture.empty?
           determine_compute_architecture
         else
-          if docker_architecture.start_with?('linux/')
-            architecture_name = docker_architecture.split('/')[1]
+          raise "Missing 'linux/' prefix in DOCKER_ARCHITECTURE: #{docker_architecture}" unless docker_architecture.start_with?('linux/')
 
-            if ALLOWED_ARCHITECTURES.include?(architecture_name)
-              docker_architecture
-            else
-              raise "Invalid DOCKER_ARCHITECTURE: #{architecture_name}. Allowed architectures are #{ALLOWED_ARCHITECTURES.join(', ')}"
-            end
-          else
-            raise "Missing 'linux/' prefix in DOCKER_ARCHITECTURE: #{docker_architecture}"
+          architecture_name = docker_architecture.split('/')[1]
+
+          unless ALLOWED_ARCHITECTURES.include?(architecture_name)
+            raise "Invalid DOCKER_ARCHITECTURE: #{architecture_name}. Allowed architectures are #{ALLOWED_ARCHITECTURES.join(', ')}"
           end
+
+          docker_architecture
+
         end
       end
     end
