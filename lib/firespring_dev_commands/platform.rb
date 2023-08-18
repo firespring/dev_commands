040824779dd928997761acb75c36a0f11820dbea
Ron Gallagher <ron.gallagher@firespring.com>
2023-08-18 07:58:22 -0500

    [FDP-45531] Update Culture Platform Build Process

---
+++ lib/firespring_dev_commands/platform.rb
@@ -0,0 +1,37 @@
+module Dev
+  class Common
+    class Platform
+      ALLOWED_ARCHITECTURES = %w[arm64 amd64]
+
+      def determine_compute_architecture
+        case RUBY_PLATFORM
+        when /x86_64|amd64/
+          'linux/amd64' # 64-bit Intel/AMD architecture
+        when /arm|aarch64/
+          'linux/arm64' # ARM architecture
+        else
+          raise "Unknown or unsupported architecture"
+        end
+      end
+
+      def architecture
+        docker_architecture = ENV['DOCKER_ARCHITECTURE'].to_s.strip.downcase
+        if docker_architecture.empty?
+          determine_compute_architecture
+        else
+          if docker_architecture.start_with?('linux/')
+            architecture_name = docker_architecture.split('/')[1]
+
+            if ALLOWED_ARCHITECTURES.include?(architecture_name)
+              docker_architecture
+            else
+              raise "Invalid DOCKER_ARCHITECTURE: #{architecture_name}. Allowed architectures are #{ALLOWED_ARCHITECTURES.join(', ')}"
+            end
+          else
+            raise "Missing 'linux/' prefix in DOCKER_ARCHITECTURE: #{docker_architecture}"
+          end
+        end
+      end
+    end
+  end
+end
