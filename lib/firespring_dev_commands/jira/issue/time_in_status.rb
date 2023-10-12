aed5d09ed2be19035aa049e839d5e05eac59048f
Joe Ebmeier <joe.ebmeier@firespring.com>
2023-10-12 08:41:08 -0500

    WIP

---
+++ lib/firespring_dev_commands/jira/issue/time_in_status.rb
@@ -0,0 +1,84 @@
+require 'base64'
+require 'net/http'
+
+module Dev
+  # Class which contains methods to conenct to jira and query issues
+  class Jira
+    class Issue
+      class TimeInStatus
+        # The filename where we store the local auth information
+        CONFIG_FILE = "#{Dir.home}/.env.tis".freeze
+
+        # The name of the environmental setting which holds the slack token
+        JIRA_TIS_TOKEN = 'JIRA_TIS_TOKEN'.freeze
+
+        # The default TimeInStatus api url
+        DEFAULT_URL = 'https://tis.obss.io/'
+
+        Config = Struct.new(:token, :url, :http_debug) do
+          def initialize
+            Dotenv.load(CONFIG_FILE) if File.exist?(CONFIG_FILE)
+
+            self.token = ENV.fetch(JIRA_TIS_TOKEN, nil)
+            self.url = DEFAULT_URL
+            self.http_debug = false
+          end
+        end
+
+        class << self
+          # Instantiates a new top level config object if one hasn't already been created
+          # Yields that config object to any given block
+          # Returns the resulting config object
+          def config
+            @config ||= Config.new
+            yield(@config) if block_given?
+            @config
+          end
+
+          # Alias the config method to configure for a slightly clearer access syntax
+          alias_method :configure, :config
+        end
+
+        attr_accessor :username, :token, :url, :client
+
+        # Initialize a new jira client using the given inputs
+        def initialize(token: self.class.config.token, url: self.class.config.url)
+          @token = token
+          @url = url
+
+          uri = URI.parse(url)
+          @client = Net::HTTP.new(uri.host, uri.port)
+          @client.use_ssl = true
+          @client.verify_mode = OpenSSL::SSL::VERIFY_PEER
+          @client.set_debug_output(LOG) if self.class.config.http_debug
+        end
+
+        def headers(addtl_headers = {})
+          {Authorization: "TISJWT #{token}"}.merge(addtl_headers)
+        end
+
+        def get(path, args: {})
+          # TODO: Figure out how to add args
+
+          response = client.request_get(path, headers)
+          puts response.inspect
+
+          # Raise an error if we got a non-success from TIS
+          raise "error response from TimeInStatus (#{response.code}): #{response.message}" unless response.is_a?(Net::HTTPSuccess)
+
+          puts "RESPONSE IS #{response.body.inspect}"
+
+          tis_data = JSON.parse(response.body)
+
+          #        rescue => e
+          #          LOG.error("Error looking up severity for #{cve}: #{e.message}")
+          #          LOG.error('WARNING: Unable to determine severity - ignoring with UNKNOWN')
+        end
+
+        def list(jql = '')
+          get('/rest/list')
+        end
+      end
+    end
+  end
+end
