899923349836192b8f432278004d95954f979acc
Joe Ebmeier <joe.ebmeier@firespring.com>
2023-11-09 09:20:40 -0600

    WIP

--- lib/firespring_dev_commands/templates/workflow.rb
+++ lib/firespring_dev_commands/templates/workflow.rb
@@ -1,18 +1,12 @@
-# TODO: Need custom pre/post hooks
-# TODO: Need bypass options?
-
 require_relative 'base_interface'
 
 module Dev
   module Template
     class Workflow < Dev::Template::BaseInterface
-      attr_reader :prefix, :workflow
+      attr_reader :workflow
 
       # Create workflow templated commands
-      # If no workflow is given it will use a default one with "none" templates
-      # If no prefix is given it will default to 'feature'
-      def initialize(workflow: nil, prefix: nil, exclude: [])
-        @prefix = prefix || 'feature'
+      def initialize(workflow: nil, exclude: [])
         @workflow = workflow || Dev::Workflow::Default.new
 
         raise "workflow must be a workflow" unless @workflow.is_a?(Dev::Workflow::Base)
@@ -23,11 +17,10 @@ module Dev
       def create_start_task!
         # Have to set a local variable to be accessible inside of the instance_eval block
         exclude = @exclude
-        prefix = @prefix
         workflow = @workflow
 
         DEV_COMMANDS_TOP_LEVEL.instance_eval do
-          namespace prefix do
+          namespace workflow.prefix do
             return if exclude.include?(:start)
 
             # TODO Add what variables you can pass in to configure your workflow
@@ -43,11 +36,10 @@ module Dev
       def create_review_task!
         # Have to set a local variable to be accessible inside of the instance_eval block
         exclude = @exclude
-        prefix = @prefix
         workflow = @workflow
 
         DEV_COMMANDS_TOP_LEVEL.instance_eval do
-          namespace prefix do
+          namespace workflow.prefix do
             return if exclude.include?(:review)
 
             desc 'Perform the "review" workflow' \
@@ -63,11 +55,10 @@ module Dev
       def create_delete_task!
         # Have to set a local variable to be accessible inside of the instance_eval block
         exclude = @exclude
-        prefix = @prefix
         workflow = @workflow
 
         DEV_COMMANDS_TOP_LEVEL.instance_eval do
-          namespace prefix do
+          namespace workflow.prefix do
             return if exclude.include?(:delete)
 
             desc 'Perform the "delete" workflow' \
@@ -82,11 +73,10 @@ module Dev
       def create_finish_task!
         # Have to set a local variable to be accessible inside of the instance_eval block
         exclude = @exclude
-        prefix = @prefix
         workflow = @workflow
 
         DEV_COMMANDS_TOP_LEVEL.instance_eval do
-          namespace prefix do
+          namespace workflow.prefix do
             return if exclude.include?(:finish)
 
             desc 'Perform the "finish" workflow' \
@@ -97,104 +87,6 @@ module Dev
           end
         end
       end
-
-
-
-
-
-
-
-
-=begin
-# start
-    raise "Unable to authenticate with TargetProcess. Check your credentials" unless TargetProcess.new.authenticated?
-    raise 'Unable to authenticate with GitHub. Check your credentials' unless GitHub.new.authenticated?
-
-    # 1.) Check WIP limits / prerequisites
-    # 2.) Use gitflow to start the story branch (specify alt base branch)
-
-    # If story was queried
-      # 3.) Update the story to "In Progress"
-      # 4.) Assign user and team to the story
-      # 5.) Add auto-tasks to a story????
-    # else
-      # Log warning
-=end
-
-=begin
-# pr / review
-    raise 'Unable to authenticate with TargetProcess. Check your credentials' unless TargetProcess.new.authenticated?
-    raise 'Unable to authenticate with GitHub. Check your credentials' unless GitHub.new.authenticated?
-
-    # 1.) Check WIP limits / prerequisites
-
-    # if story was queried
-        # 2.) Require phase 1 and 2 SDL tasks be complete
-        # 3.) Require all dev tasks be complete
-
-    # 4.) Merge base branch into the story branch to make sure it is up to date (try to honor alt base branch)
-
-    if story was queried
-      # 5.) Update the story to "In Testing"
-    else
-      # Log warning
-
-    # 6.) Open a PR for this branch
-      # if pr exists make sure it is open
-      # else create a new PR
-      # Create a new PR
-
-    # 6.5) Notify slack if configured
-    # 7.) Add labels to the PR if desired
-    # 8.) Add user's dev team label to the PR
-    # 9.) Create CI unless ENV['NO_CI'] == 'true'
-=end
-
-=begin
-# delete
-    raise "Unable to authenticate with TargetProcess. Check your credentials" unless TargetProcess.new.authenticated?
-    raise 'Unable to authenticate with GitHub. Check your credentials' unless GitHub.new.authenticated?
-    raise 'The only valid values for FORCE_DELETE are blank and "true"' if ENV['FORCE_DELETE'] && ENV['FORCE_DELETE'] != 'true'
-
-    # 1.) Delete the git branch (with confirmation)
-
-    if story was queried
-      # 2.) Move the story to "Done"? Or do we delete it? 
-
-    # 3.) Close the PR
-    # 4.) Remove a beta stack if one exists
-    # 6.) Remove any docker images for this branch
-    # 7.) Remove codepipeline if one exists
-=end
-
-=begin
-# finish
-    raise 'Unable to authenticate with TargetProcess. Check your credentials' unless TargetProcess.new.authenticated?
-    raise 'Unable to authenticate with GitHub. Check your credentials' unless GitHub.new.authenticated?
-
-    %w[NO_SQUASH NO_CI NO_RELEASE_CLOSED NO_RELEASE_MATCH NO_BETA_REMOVE NO_PR].each do |x|
-      raise "The only valid values for #{x} are blank or \"true\"" if ENV[x] && !ENV[x].casecmp?('true')
-    end
-
-    if story was queried
-      # 1.) Require all tasks on the story to be complete
-
-    # 2.) Require codepipeline builds to all have passed
-    # 3.) Require the PR to have the correct signoffs
-    # 4.) Require the release to exist and it's end date to be in the future
-    # 5.) Require the release already defined on the story, if exists, to match the release we're finishing into
-    # 6.) Merge the git branch in to the release (with confirmation)
-
-    if story was queried
-      # 7.) Move the story to "QA Pending"
-      # 8.) Assign the story to the release it was merged in to
-
-    # 9.) Close the PR
-    # 10.) Remove a beta stack if one exists
-    # 12.) Remove any docker images for this branch
-    # 13.) Remove story codepipeline if one exists
-=end
-
     end
   end
 end
--- lib/firespring_dev_commands/workflow/default.rb
+++ lib/firespring_dev_commands/workflow/default.rb
@@ -1,12 +1,8 @@
 module Dev
   module Workflow
     class Default < Base
-      Config = Struct.new(:project, :source_control, :code_review, :continuous_integration, :prefix) do
+      Config = Struct.new(:prefix) do
         def initialize
-          self.project = nil
-          self.source_control = nil
-          self.code_review = nil
-          self.continuous_integration = nil
           self.prefix = nil
         end
       end
@@ -25,16 +21,12 @@ module Dev
         alias_method :configure, :config
       end
 
-      attr_accessor :branch, :project, :source_control, :code_review, :continuous_integration
+      attr_accessor :branch
 
       def initialize(
-        branch: nil,
-        project: self.class.config.project,
-        source_control: self.class.config.source_control,
-        code_review: self.class.config.code_review,
-        continuous_integration: self.class.config.continuous_integration
+        branch: nil
+        prefix: self.class.config.prefix
       )
-
         @prefix = prefix || 'feature'
         @branch = branch || ENV['BRANCH'].to_s.strip
         raise 'Must specify BRANCH as an environment variable' if @branch.strip.empty?
@@ -48,172 +40,28 @@ module Dev
         #@release_name ||= release_branch.sub(/release\//, '')
         #@number ||= name.downcase.match(/tp-([0-9]+).*/)&.[](1).to_i
         #@docker_tag = branch.gsub('/','_')
-
-
-
-        @project = project || Dev::Workflow::Project::None.new
-        raise "project must be a project" unless @project.is_a?(Dev::Workflow::Project::Base)
-
-        @source_control = source_control || Dev::Workflow::SourceControl::Git.new
-        raise "source control must be a source control" unless @source_control.is_a?(Dev::Workflow::SourceControl::Base)
-
-        @code_review = code_review || Dev::Workflow::CodeReview::None.new
-        raise "code review must be a code review" unless @code_review.is_a?(Dev::Workflow::CodeReview::Base)
-
-        @continuous_integration = continuous_integration || Dev::Workflow::ContinuousIntegration::None.new
-        raise "continuous integration must be a continuous integration" unless @continuous_integration.is_a?(Dev::Workflow::ContinuousIntegration::Base)
-
       end
-
-
-
-
-
-
-
-
-
-
-
 
       def start
-        # TODO: Prerequisites
-        #raise "Unable to authenticate with TargetProcess. Check your credentials" unless TargetProcess.new.authenticated?
-        #raise 'Unable to authenticate with GitHub. Check your credentials' unless GitHub.new.authenticated?
-        # 1.) Check WIP limits / prerequisites
-
-        source_control.start
-        # 2.) Use gitflow to start the story branch (specify alt base branch)
-
-        project.start
-        # If story was queried
-        # 3.) Update the story to "In Progress"
-        # 4.) Assign user and team to the story
-        # 5.) Add auto-tasks to a story????
-        # else
-        # Log warning
-
-        code_review.start
-
-        continuous_integration.start
-        # Nothing
+        # TODO: Add confirmation
+        #Dev::Git.new.create_branch_all
       end
 
-      # Audit? Test? 
       def review
-        # TODO: Prerequisites
-        #raise 'Unable to authenticate with TargetProcess. Check your credentials' unless TargetProcess.new.authenticated?
-        #raise 'Unable to authenticate with GitHub. Check your credentials' unless GitHub.new.authenticated?
-        # 1.) Check WIP limits / prerequisites
-        # if story was queried
-        # 2.) Require phase 1 and 2 SDL tasks be complete
-        # 3.) Require all dev tasks be complete
-
-        source_control.review
-        # 4.) Merge base branch into the story branch to make sure it is up to date (try to honor alt base branch)
-
-        project.review
-        #if story was queried
-        #  # 5.) Update the story to "In Testing"
-        #else
-        #  # Log warning
-
-        code_review.review
-
-        continuous_integration.review
+        # TODO: Add confirmation
+        # TODO: Merge in staging branch?
       end
 
       def delete
-        # TODO: Prerequisites
-        #raise "Unable to authenticate with TargetProcess. Check your credentials" unless TargetProcess.new.authenticated?
-        #raise 'Unable to authenticate with GitHub. Check your credentials' unless GitHub.new.authenticated?
-        #raise 'The only valid values for FORCE_DELETE are blank and "true"' if ENV['FORCE_DELETE'] && ENV['FORCE_DELETE'] != 'true'
-
-        source_control.delete
-        # 1.) Delete the git branch (with confirmation)
-
-        project.delete
-        #if story was queried
-        #  # 2.) Move the story to "Done"? Or do we delete it?
-
-        code_review.delete
-
-        continuous_integration.delete
-        # 7.) Remove codepipeline if one exists
-
+        # TODO: Add confirmation
+        #Dev::Git.new.delete_all
       end
 
       def finish
-        # TODO: Prerequisites
-        #raise 'Unable to authenticate with TargetProcess. Check your credentials' unless TargetProcess.new.authenticated?
-        #raise 'Unable to authenticate with GitHub. Check your credentials' unless GitHub.new.authenticated?
-
-        #%w[NO_SQUASH NO_CI NO_RELEASE_CLOSED NO_RELEASE_MATCH NO_BETA_REMOVE NO_PR].each do |x|
-        #  raise "The only valid values for #{x} are blank or \"true\"" if ENV[x] && !ENV[x].casecmp?('true')
-        #end
-        #if story was queried
-        #  # 1.) Require all tasks on the story to be complete
-        # 2.) Require codepipeline builds to all have passed
-        # 3.) Require the PR to have the correct signoffs
-        # 4.) Require the release to exist and it's end date to be in the future
-        # 5.) Require the release already defined on the story, if exists, to match the release we're finishing into
-
-        source_control.finish
-        # 6.) Merge the git branch in to the release (with confirmation)
-
-        project.finish
-        #if story was queried
-        #  # 7.) Move the story to "QA Pending"
-        #  # 8.) Assign the story to the release it was merged in to
-
-        code_review.delete
-
-
-        continuous_integration.finish
-        # 13.) Remove story codepipeline if one exists
+        # TODO: Add confirmation
+        # Need to squash merge
+        #Dev::Git.new.merge_all
       end
     end
   end
 end
-
-=begin
-# start
-
-
-=end
-
-=begin
-# pr / review
-
-
-    # 6.) Open a PR for this branch
-      # if pr exists make sure it is open
-      # else create a new PR
-      # Create a new PR
-
-    # 6.5) Notify slack if configured
-    # 7.) Add labels to the PR if desired
-    # 8.) Add user's dev team label to the PR
-    # 9.) Create CI unless ENV['NO_CI'] == 'true'
-=end
-
-=begin
-# delete
-
-
-
-    # 3.) Close the PR
-    # 4.) Remove a beta stack if one exists
-    # 6.) Remove any docker images for this branch
-=end
-
-=begin
-# finish
-
-
-
-
-    # 9.) Close the PR
-    # 10.) Remove a beta stack if one exists
-    # 12.) Remove any docker images for this branch
-=end
