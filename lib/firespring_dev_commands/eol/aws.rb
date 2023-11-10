9843dd5f248833562a2b8ef20667e316480f5c25
Joe Ebmeier <joe.ebmeier@firespring.com>
2023-11-10 07:59:47 -0600

    WIP

--- lib/firespring_dev_commands/eol/aws.rb
+++ lib/firespring_dev_commands/eol/aws.rb
@@ -8,53 +8,110 @@ module Dev
     class Aws
       def elasticache_products
         client = ::Aws::ElastiCache::Client.new
-        client.describe_cache_clusters.cache_clusters.filter_map do |cluster|
-          name = cluster.cache_cluster_id
-          product = cluster.engine
-          version = cluster.engine_version.reverse.split('.')[-2..].join('.').reverse
-          Dev::EndOfLife::ProductVersion.new(product, version, name)
+
+        [].tap do |ary|
+          query_all(client, :describe_cache_clusters) do |response|
+            response.cache_clusters.each do |cluster|
+              name = cluster.cache_cluster_id
+              product = cluster.engine
+              version = cluster.engine_version.reverse.split('.')[-2..].join('.').reverse
+              ary << Dev::EndOfLife::ProductVersion.new(product, version, name)
+            end
+          end
         end
       end
 
       def lambda_products
         client = ::Aws::Lambda::Client.new
-        client.list_functions.functions.filter_map do |function|
-          # Runtime is empty if using a docker image
-          next unless function.runtime
 
-          name = function.function_name
-          product = function.runtime.split(/[0-9]/, 2).first
-          version = function.runtime.split(/#{product}/, 2).last.chomp('.x')
-          Dev::EndOfLife::ProductVersion.new(product, version, name)
+        [].tap do |ary|
+          query_all(client, :list_functions) do |response|
+            response.functions.each do |function|
+              # Runtime is empty if using a docker image
+              # TODO Should we still handle this case?
+              next unless function.runtime
+
+              name = function.function_name
+              product = function&.runtime.split(/[0-9]/, 2).first
+              version = function&.runtime.split(/#{product}/, 2).last.chomp('.x')
+              ary << Dev::EndOfLife::ProductVersion.new(product, version, name)
+            end
+          end
         end
       end
 
       def opensearch_products
         client = ::Aws::OpenSearchService::Client.new
-        client.list_domain_names.domain_names.filter_map do |domain|
-          name = domain.domain_name
-          product = domain.engine_type
-          version = client.describe_domain(domain_name: name).domain_status.engine_version.split('_').last.split('.').first
-          Dev::EndOfLife::ProductVersion.new(product, version, name)
+
+        [].tap do |ary|
+          query_all(client, :list_domain_names) do |response|
+            response.domain_names.each do |domain|
+              name = domain.domain_name
+              product = domain.engine_type
+              version = client.describe_domain(domain_name: name).domain_status.engine_version.split('_').last.split('.').first
+              ary << Dev::EndOfLife::ProductVersion.new(product, version, name)
+            end
+          end
         end
       end
 
       def rds_products
+        _rds_instances + _rds_clusters
+      end
+
+      def _rds_instances
         aws_engines = %w(mysql postgresql)
-
         client = ::Aws::RDS::Client.new
-        client.describe_db_instances.db_instances.filter_map do |instance|
-          name = instance.db_instance_identifier
-          product = if aws_engines.include?(instance.engine)
-                      "amazon-rds-#{instance.engine}"
-                    else
-                      instance.engine
-                    end
-          version = instance.engine_version.reverse.split('.')[-2..].join('.').reverse
-          Dev::EndOfLife::ProductVersion.new(product, version, name)
-        end
 
-        # TODO: Add db cluster info too?
+        [].tap do |ary|
+          query_all(client, :describe_db_instances) do |response|
+            response.db_instances.each do |instance|
+              name = instance.db_instance_identifier
+              engine = instance.engine.tr('aurora-', '')
+              product = if aws_engines.include?(engine)
+                          "amazon-rds-#{engine}"
+                        else
+                          engine
+                        end
+              version = instance.engine_version.reverse.split('.')[-2..].join('.').reverse
+              ary << Dev::EndOfLife::ProductVersion.new(product, version, name)
+            end
+          end
+        end
+      end
+
+      def _rds_clusters
+        aws_engines = %w(mysql postgresql)
+        client = ::Aws::RDS::Client.new
+
+        [].tap do |ary|
+          query_all(client, :describe_db_clusters) do |response|
+            response.db_clusters.each do |cluster|
+              name = cluster.db_cluster_identifier
+              engine = cluster.engine.tr('aurora-', '')
+              product = if aws_engines.include?(engine)
+                          "amazon-rds-#{engine}"
+                        else
+                          engine
+                        end
+              version = cluster.engine_version.reverse.split('.')[-2..].join('.').reverse
+              ary << Dev::EndOfLife::ProductVersion.new(product, version, name)
+            end
+          end
+        end
+      end
+
+      def query_all(client, query, params = {})
+        response = client.send(query, params)
+        yield response
+
+        next_token = response.next_token if response.respond_to?(:next_token)
+        while next_token
+          params[:next_token] = next_token
+          response = client.send(query, params)
+          yield response
+          next_token = response.next_token
+        end
       end
     end
   end
