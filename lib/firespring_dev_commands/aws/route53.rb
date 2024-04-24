86eead83113725fd6497fe92ce66b609a5235d44
Ron Gallagher <ron.gallagher@firespring.com>
2024-04-24 09:58:41 -0500

    Update

--- lib/firespring_dev_commands/aws/route53.rb
+++ lib/firespring_dev_commands/aws/route53.rb
@@ -21,7 +21,7 @@ module Dev
         [].tap do |ary|
           Dev::Aws.each_page(client, :list_hosted_zones) do |response|
             response.hosted_zones&.each do |hosted_zone|
-              ary << hosted_zone.id unless hosted_zone.config.private_zone
+              ary << hosted_zone unless hosted_zone.config.private_zone
             end
           end
         end
@@ -34,7 +34,7 @@ module Dev
             target = response.hosted_zones.find { |it| it.name.chomp('.') == domain_name }
             raise "The #{domain_name} hosted zone not found." unless target
 
-            ary << target.id
+            ary << target
           end
         end
       end
@@ -46,37 +46,61 @@ module Dev
         ).query_logging_configs&.first&.id
       end
 
+      def pretty_puts(output)
+        # Find the maximum length of the keys
+        max_key_length = output.keys.map(&:to_s).max_by(&:length).length
+
+        output.each do |key, value|
+          puts "#{key.to_s.ljust(max_key_length)}\t=>\t#{value}"
+        end
+      end
+
+      def list_query_configs
+        output = {}
+        @zones.each do |zone|
+          target_config_id = target_config_id(zone.id)
+
+          output[zone.name] = if target_config_id
+                                "Config\t=>\t#{target_config_id}".colorize(:green)
+                              else
+                                'No query logging config assigned.'.colorize(:red)
+                              end
+        end
+
+        pretty_puts(output)
+      end
+
       def activate_query_logging(log_group)
         output = {}
 
         @zones.each do |zone|
           response = client.create_query_logging_config(
-            hosted_zone_id: zone,
+            hosted_zone_id: zone.id,
             cloud_watch_logs_log_group_arn: log_group
           )
-          output[zone] = response.location
+          output[zone.id] = response.location
         rescue ::Aws::Route53::Errors::ServiceError => e
           raise "Error: #{e.message}" unless e.instance_of?(::Aws::Route53::Errors::QueryLoggingConfigAlreadyExists)
 
-          output[zone] = e.message
+          output[zone.id] = e.message
         end
-        pp output
+        pretty_puts(output)
       end
 
       def deactivate_query_logging
         output = {}
         @zones.each do |zone|
-          target_config_id = target_config_id(zone)
+          target_config_id = target_config_id(zone.id)
           if target_config_id
             client.delete_query_logging_config(
               id: target_config_id
             )
-            output[zone] = 'Query logging config removed.'
+            output[zone.id] = 'Query logging config removed.'.colorize(:green)
           else
-            output[zone] = 'No query logging config assigned.'
+            output[zone.id] = 'No query logging config assigned.'.colorize(:red)
           end
         end
-        pp output
+        pretty_puts(output)
       end
     end
   end
---
+++ lib/firespring_dev_commands/templates/aws/services/route53.rb
@@ -0,0 +1,105 @@
+require_relative '../../base_interface'
+
+module Dev
+  module Template
+    class Aws
+      module Services
+        # Class contains rake templates for managing your AWS settings and logging in
+        class Route53 < Dev::Template::BaseInterface
+          # Create the rake task which ensures active credentials are present
+          def create_ensure_credentials_task!
+            # Have to set a local variable to be accessible inside of the instance_eval block
+            exclude = @exclude
+
+            DEV_COMMANDS_TOP_LEVEL.instance_eval do
+              return if exclude.include?(:ensure_aws_credentials)
+
+              task ensure_aws_credentials: %w(init) do
+                raise 'AWS Credentials not found / expired' unless Dev::Aws::Credentials.new.active?
+              end
+            end
+          end
+
+          # Create the rake task for the hosted zone method
+          def create_dns_logging_activate_task!
+            # Have to set a local variable to be accessible inside of the instance_eval block
+            exclude = @exclude
+
+            DEV_COMMANDS_TOP_LEVEL.instance_eval do
+              namespace :aws do
+                return if exclude.include?(:dns_logging)
+
+                namespace :hosted_zone do
+                  namespace :dns_logging do
+                    desc 'Activates query logging for all hosted zones by default.' \
+                         'This command should be run from the account the hosted zone(s) reside.' \
+                         "\n\toptionally specify HOSTED_ZONE_GROUP='arn:aws:logs:REGION:ACCOUNT_ID:' to specify the ARN of the target log group." \
+                         "\n\toptionally specify DOMAINS='foo.com,foobar.com' to specify the hosted zones to activate." \
+                         "\n\t\tComma delimited list."
+                    task activate: %w(ensure_aws_credentials) do
+                      route53 = Dev::Aws::Route53.new
+                      route53.zones(ENV['DOMAINS'].to_s.strip.split(','))
+                      # Use user defined log group. Otherwise, go get the default.
+                      log_group = (ENV['HOSTED_ZONE_GROUP'] || Dev::Aws::Parameter.new.get_value('/Firespring/Internal/Route53/hosted-zone/log-group-arn'))
+                      route53.activate_query_logging(log_group)
+                    end
+                  end
+                end
+              end
+            end
+          end
+
+          # Create the rake task for the hosted zone method
+          def create_dns_logging_deactivate_task!
+            # Have to set a local variable to be accessible inside of the instance_eval block
+            exclude = @exclude
+
+            DEV_COMMANDS_TOP_LEVEL.instance_eval do
+              namespace :aws do
+                return if exclude.include?(:dns_logging_de)
+
+                namespace :hosted_zone do
+                  namespace :dns_logging do
+                    desc 'Deactivates query logging for all hosted zones by default. ' \
+                         'This command should be run from the account the hosted zone(s) reside.' \
+                         "\n\toptionally specify DOMAINS='foo.com,foobar.com' to specify the hosted zones to activate." \
+                         "\n\t\tComma delimited list."
+                    task deactivate: %w(ensure_aws_credentials) do
+                      route53 = Dev::Aws::Route53.new
+                      route53.zones(ENV['DOMAINS'].to_s.strip.split(','))
+                      route53.deactivate_query_logging
+                    end
+                  end
+                end
+              end
+            end
+          end
+
+          # Create the rake task for the hosted zone method
+          def create_list_query_config_task!
+            # Have to set a local variable to be accessible inside of the instance_eval block
+            exclude = @exclude
+
+            DEV_COMMANDS_TOP_LEVEL.instance_eval do
+              namespace :aws do
+                return if exclude.include?(:dns_logging_ls)
+
+                namespace :hosted_zone do
+                  desc 'Deactivates query logging for all hosted zones by default. ' \
+                       'This command should be run from the account the hosted zone(s) reside.' \
+                       "\n\toptionally specify DOMAINS='foo.com,foobar.com' to specify the hosted zones to activate." \
+                       "\n\t\tComma delimited list."
+                  task list_query_configs: %w(ensure_aws_credentials) do
+                    route53 = Dev::Aws::Route53.new
+                    route53.zones(ENV['DOMAINS'].to_s.strip.split(','))
+                    route53.list_query_configs
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
