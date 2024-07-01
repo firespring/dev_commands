4affb8d4c442b72a927f1736cc1e772ff9c0e3db
Joe Ebmeier <joe.ebmeier@firespring.com>
2024-07-01 08:03:05 -0500

    WIP

--- lib/firespring_dev_commands/dns/resource.rb
+++ lib/firespring_dev_commands/dns/resource.rb
@@ -1,49 +1,70 @@
 module Dev
   class Dns
     class Resource
-      attr_reader :domain, :ip_address
+      attr_reader :domain
 
-      def initialize(ip_or_domain)
-        if ip?(ip_or_domain)
-          @domain = nil
-          @ip_address = ip_or_domain
-        else
-          @domain = ip_or_domain
-          @ip_address = nil
-        end
+      def initialize(domain)
+        @domain = domain
       end
 
-      private def ip?(value)
-        value.match?(Resolv::IPv4::Regex) || value.match?(Resolv::IPv6::Regex)
+      # Returns whether or not the given value is a valid IPv4 or IPv6 address
+      def self.ip?(value)
+        ipv4?(value) || ipv6?(value)
       end
 
-      # If we don't have an IP address, that means the user passed in a domain.
-      # Look it up now since the user wants the IP address
-      def ip_address
-        @ip_address ||= lookup(domain, type: Resolv::DNS::Resource::IN::A)
+      # Returns whether or not the given value is a valid IPv4 address
+      def self.ipv4?(value)
+        value.match?(Resolv::IPv4::Regex)
       end
 
+      # Returns whether or not the given value is a valid IPv6 address
+      def self.ipv6?(value)
+        value.match?(Resolv::IPv6::Regex)
+      end
+
+      # Recurisively determine the correct nameservers for the given domain.
+      # If nameservers are not found, strip subdomains off until we've reached the TLD
       def recursive_nameserver_lookup(name = domain)
-        type = Resolv::DNS::Resource::IN::NS
-        records = lookup(name, type:)
+        records = lookup(name, type: Resolv::DNS::Resource::IN::NS)
 
         # Strip the subdomain and try again if we didn't find any nameservers (this can happen with wildcards)
         return recursive_nameserver_lookup(name.split('.', 2).last) if records.empty?
 
-        records
+        # Look up the IPs for the nameservers
+        records.map { |name| recursive_cname_or_a_lookup(name) }
+      end
+
+      # Recursively attempt to find an A record for the given domain.
+      # If one isn't found, also check for CNAMEs continually until we have either found an IP or run out of things to check
+      def recursive_a_lookup(name = domain)
+        # Try looking up an A record first. If we find one, we are done.
+        records = lookup(name, type: Resolv::DNS::Resource::IN::A)
+        return records unless records.empty?
+
+        # Try looking up a CNAME record
+        records = lookup(name, type: Resolve::DNS::Resource::IN::CNAME)
+
+        # If we didn't find an A record _or_ a CNAME, just return empty
+        return records if records.empty?
+
+        # If we found more than one CNAME that is a DNS error
+        raise "Found more than one CNAME entry for #{name}. This is not allowed by DNS" if records.length > 1
+
+        return recursive_cname_or_a_lookup(records.first)
       end
 
       # Lookup the given name using the record type provided.
-      # 
       def lookup(name = domain, type: Resolv::DNS::Resource::IN::A)
-        raise 'lookup type must be a Resolve::DNS::IN' unless type.ancestors.include?(Resolv::DNS::Resource)
+        # Validate the type
+        raise 'lookup type must be a Resolve::DNS::Resource' unless type.ancestors.include?(Resolv::DNS::Resource)
 
-        # If we've stripped subdomains to the point where we are at a tld
+        # If we were given a tld, return empty
         return [] unless name.include?('.')
 
         # Look up NS records for the given host
         records = Resolv::DNS.new.getresources(name, type)
 
+        # Return the record names
         records.map { |record| record.name.to_s }
       rescue
         []
